9e197fba3b3d3c3e521ef8eedc133581e99b05fb
Michael Martinez <zeroeclipse@live.com>
2020-12-27 02:35:33 -0800

    commit tree cfc45ce8f253416bd96fa8cca7f43e27d9ab2f3f

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,63 @@
 require_relative "../config/environment.rb"
 
 class Student
-
+  attr_accessor :name, :grade
+  attr_reader :id
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
+def initialize(id = nil, name, grade)
+  @id = id
+  @name = name
+  @grade = grade
+end
+
+def self.create_table
+  sql = "CREATE TABLE students (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    grade INTEGER
+    );"
+    DB[:conn].execute(sql)
+end
+
+def self.drop_table
+sql = "DROP TABLE students;"
+DB[:conn].execute(sql)
+end
+
+def save
+  if self.id
+    self.update
+  else
+    sql = "INSERT INTO students(name, grade) VALUES (?, ?)"
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
+end
+
+def self.create(name, grade)
+  student = Student.new(name, grade)
+  student.save
+end
+
+def self.new_from_db(row)
+  new_student = self.new(row[0], row[1], row[2])
+end
+
+def self.find_by_name(name)
+  sql = "SELECT * FROM students WHERE students.name = ?;"
+  array = DB[:conn].execute(sql, name)[0]
+  Student.new(array[0], array[1],array[2])
+end
+
+def update
+  sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?;"
+  DB[:conn].execute(sql, self.name, self.grade, self.id)
+end
 
 end
+
+
+
+
